c8df9d26eb12b89b234f01022da58dd0201d0f5e
pasoiualex <firstalex93@gmail.com>
2022-06-01 16:06:15 +0300

    commit 2

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -15,8 +15,20 @@ export default function Post() {
                   <MoreVert/>
               </div>
           </div>
-          <div className="postCenter"></div>
-          <div className="postBotton"></div>
+          <div className="postCenter">
+            <span className="postText">Hey! its my first post :)</span>
+            <img src="/assets/post/1.jpg" alt="" className="postImg" />
+          </div>
+          <div className="postBottom">
+            <div className="postBottomLeft">
+              <img src="/assets/like.png" alt="" className="likeIcon" />
+              <img src="/assets/heart.png" alt="" className="likeIcon" />
+              <span className="postLikeCounter">32 de persoane</span>
+            </div>
+            <div className="postBottomRight">
+              <span className="postCommentText">5 coemntarii</span>
+            </div>
+          </div>
       </div>
     </div>
   );
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,61 @@ import "./rightbar.css"
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/birthday.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Pasoiu Alex</b> si <b> inca  3 priteni </b> isi sarbatoresc ziua astazi !
+          </span>
+        </div>
+        <img src="/assets/Ad.jpeg" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Miruna Serban</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Miruna Serban</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Miruna Serban</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Miruna Serban</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Miruna Serban</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Miruna Serban</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
